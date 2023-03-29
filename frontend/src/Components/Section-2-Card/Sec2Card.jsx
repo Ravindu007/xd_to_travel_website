df9c398e9e39b8e48e1ee4da715655990d16a053
Ravindu007 <ravindu0504@gmail.com>
2023-03-30 00:19:58 +0530

    styles

--- frontend/src/Components/Section-2-Card/Sec2Card.jsx
+++ frontend/src/Components/Section-2-Card/Sec2Card.jsx
@@ -3,13 +3,9 @@ import "./card.scss"
 
 const Sec2Card = ({card}) => {  
   return (
-    <div className="col-sm-12 col-md-3">
+    <div className="col-sm-12 col-md-3" style={{display:"flex", justifyContent:"center"}}>
       <div className="card" style={{ height:"347px", width:"267px"}}>
-        <div className="card-body" style={{display:"flex", flexDirection:"column", justifyContent:"center"}}>
-        <span> </span>
-        <span> </span>
-        <span> </span>
-        <span> </span>
+        <div className="card-body" style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
           <img src={card.imgSrc} alt="" className='mx-auto d-block img-fluid' />
           <p>{card.title}</p>
           <p>{card.details}</p>
