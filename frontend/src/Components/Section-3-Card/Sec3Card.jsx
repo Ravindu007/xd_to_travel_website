7e66c614ca57f739a118bc6e200e046570ea230b
Ravindu007 <ravindu0504@gmail.com>
2023-03-30 17:25:12 +0530

    sec3Card - vorder annimation

--- frontend/src/Components/Section-3-Card/Sec3Card.jsx
+++ frontend/src/Components/Section-3-Card/Sec3Card.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./card.scss"
 
 const Sec3Card = ({location}) => {
+
   return (
     <div className='sec3Card col-sm-12 col-md-4'>
         <div className="card-3">
@@ -9,7 +10,7 @@ const Sec3Card = ({location}) => {
             <img src={location.imgSrc} alt="" className='card-img-top' /> 
           </div>
           <div className="card-footer">
-            <div className="col-12">
+            <div className="col-12 location-price">
               <p>{location.place}</p>
               <p>{location.price}</p>
             </div>
